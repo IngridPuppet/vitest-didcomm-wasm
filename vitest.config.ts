d8bfca073edcdb18bbf55448fc2db8075132e847
Ingrid Kamga <Ingrid.Kamga@adorsys.com>
2024-11-06 10:47:20 +0100

    Configure vitest for inline wasm support

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,12 @@ import wasm from "vite-plugin-wasm";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [wasm()]
+  plugins: [wasm()],
+  test: {
+    server: {
+      deps: {
+        inline: [/\.wasm$/],
+      },
+    },
+  },
 });
